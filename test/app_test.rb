ea6dd6c4a0b8468cfbe14f5605b526d37b06a962
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-12-13 14:21:11 -0200

    test: include tests for user creation and search

--- test/app_test.rb
+++ test/app_test.rb
@@ -16,4 +16,14 @@ class MyAppTest < Test::Unit::TestCase
     assert last_response.ok?
     assert_equal 'Hello World!', last_response.body
   end
+
+  def test_get_user
+    get '/users/1'
+    assert last_response.ok?
+  end
+
+  def test_post_user
+    post '/users'
+    assert last_response.ok?
+  end
 end
